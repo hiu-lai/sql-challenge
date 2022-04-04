e3d034ebd44152bb1696ee28b76e8965bc723ddc
hiu-lai <97108762+hiu-lai@users.noreply.github.com>
2022-04-05 00:43:57 +1000

    adding sql files

---
+++ EmployeeSQL/emp_analysis.sql
@@ -0,0 +1,39 @@
+
+-- List the following details of each employee: employee number, last name, first name, sex, and salary.
+DROP VIEW Emp_mast;
+
+CREATE VIEW Emp_mast AS (
+	SELECT employees.emp_no, last_name, first_name, sex, salary FROM employees INNER JOIN salaries ON employees.emp_no=salaries.emp_no);
+
+-- List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date FROM employees WHERE TO_CHAR(hire_date, 'yyyy') = '1986';
+
+-- List the manager of each department with the following information: department number, 
+-- department name, the manager's employee number, last name, first name.
+SELECT dept_no, 
+	(SELECT dept_name FROM departments WHERE dept_manager.dept_no = departments.dept_no), 
+	dept_manager.emp_no, last_name, first_name FROM dept_manager  JOIN employees ON dept_manager.emp_no=employees.emp_no;
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, last_name, first_name,
+	(SELECT dept_name FROM departments WHERE dept_emp.dept_no = departments.dept_no)
+	 FROM dept_emp  JOIN employees ON dept_emp.emp_no=employees.emp_no;
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex FROM employees WHERE (first_name='Hercules' AND LEFT(last_name, 1)='B') ORDER BY last_name;
+--SELECT first_name, last_name, sex FROM employees WHERE (first_name='Hercules' AND last_name='B%') ORDER BY last_name;
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+SELECT employees.emp_no, last_name, first_name, (SELECT dept_name FROM departments WHERE dept.dept_no = departments.dept_no) 
+	FROM employees JOIN (SELECT * FROM dept_emp WHERE dept_no IN (
+			SELECT dept_no FROM departments WHERE dept_name='Sales')) as dept ON employees.emp_no = dept.emp_no;
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT employees.emp_no, last_name, first_name, (SELECT dept_name FROM departments WHERE dept.dept_no = departments.dept_no) 
+	FROM employees JOIN (SELECT * FROM dept_emp WHERE dept_no IN (
+			SELECT dept_no FROM departments WHERE dept_name='Sales' OR dept_name='Development')) as dept ON employees.emp_no = dept.emp_no;
+
+-- List the frequency count of employee last names (i.e., how many employees share each last name) in descending order.
+SELECT last_name, count(last_name) as "name count" FROM employees group by last_name ORDER BY "name count" DESC;
+
--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,24 +1,15 @@
-ALTER DATABASE "sql-challenge" SET datestyle TO 'ISO, MDY';
-
-DROP TABLE departments;
-DROP TABLE dept_emp;
-DROP TABLE dept_manager;
-DROP TABLE employees;
-DROP TABLE salaries;
-DROP TABLE titles;
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
 
 CREATE TABLE IF NOT EXISTS titles (
     title_id VARCHAR   NOT NULL,
     title VARCHAR   NOT NULL,
     PRIMARY KEY (title_id)
 );
-
-DELETE FROM titles;
-
-COPY titles(title_id, title)
-FROM 'C:\Users\hk_la\Documents\homeworks\sql-challenge\EmployeeSQL\titles.csv'
-DELIMITER ','
-CSV HEADER;
 
 CREATE TABLE IF NOT EXISTS employees (
     emp_no INT   NOT NULL,
@@ -32,26 +23,11 @@ CREATE TABLE IF NOT EXISTS employees (
     PRIMARY KEY (emp_no)
 );
 
-DELETE FROM employees;
-
-COPY employees 
-FROM 'C:\Users\hk_la\Documents\homeworks\sql-challenge\EmployeeSQL\employees.csv'
-DELIMITER ','
-CSV HEADER;
-
 CREATE TABLE IF NOT EXISTS departments (
     dept_no VARCHAR   NOT NULL,
     dept_name VARCHAR   NOT NULL,
     PRIMARY KEY (dept_no)
 );
-
-DELETE FROM departments;
-
-COPY departments(dept_no, dept_name)
-FROM 'C:\Users\hk_la\Documents\homeworks\sql-challenge\EmployeeSQL\departments.csv'
-DELIMITER ','
-CSV HEADER;
-
 CREATE TABLE IF NOT EXISTS dept_manager (
     dept_no VARCHAR   NOT NULL,
     emp_no INT   NOT NULL,
@@ -59,13 +35,6 @@ CREATE TABLE IF NOT EXISTS dept_manager (
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (dept_no, emp_no)
 );
-
-DELETE FROM dept_manager;
-
-COPY dept_manager
-FROM 'C:\Users\hk_la\Documents\homeworks\sql-challenge\EmployeeSQL\dept_manager.csv'
-DELIMITER ','
-CSV HEADER;
 
 CREATE TABLE IF NOT EXISTS dept_emp (
     emp_no INT   NOT NULL,
@@ -75,13 +44,6 @@ CREATE TABLE IF NOT EXISTS dept_emp (
     PRIMARY KEY (emp_no, dept_no)
 );
 
-DELETE FROM dept_emp;
-
-COPY dept_emp 
-FROM 'C:\Users\hk_la\Documents\homeworks\sql-challenge\EmployeeSQL\dept_emp.csv'
-DELIMITER ','
-CSV HEADER;
-
 CREATE TABLE IF NOT EXISTS salaries (
     emp_no INT   NOT NULL,
     salary INT   NOT NULL,
@@ -89,11 +51,4 @@ CREATE TABLE IF NOT EXISTS salaries (
 	PRIMARY KEY (emp_no)
 );
 
-DELETE FROM salaries;
-
-COPY salaries(emp_no, salary)
-FROM 'C:\Users\hk_la\Documents\homeworks\sql-challenge\EmployeeSQL\salaries.csv'
-DELIMITER ','
-CSV HEADER;
-
-
+ALTER DATABASE "employees_files" SET datestyle TO "ISO, MDY";
